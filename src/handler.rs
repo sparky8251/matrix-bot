1d4df09abe67c25ba2ee28f457b49694d9b2ac4c
sparky8251 <sparky@possumlodge.me>
2020-04-06 19:31:33 -0400

    Initial implementation of correction response completed.

--- src/handler.rs
+++ src/handler.rs
@@ -8,6 +8,9 @@ use ruma_identifiers::{RoomId, UserId};
 use crate::error::{Error, Result};
 use crate::session::SavedSession;
 
+static INSENSITIVE_SPELL_CHECK: &'static [&'static str] = &["Jellyfish", "Jelly Fin"];
+static SENSITIVE_SPELL_CHECK: &'static [&'static str] = &["JellyFin", "jellyFin"];
+
 pub async fn handle_text_message(
     text: &TextMessageEventContent,
     sender: &UserId,
@@ -16,50 +19,78 @@ pub async fn handle_text_message(
     session: &mut SavedSession,
 ) -> Result<()> {
     if !text.body.starts_with('!') {
-        correct_spelling(text, sender, room_id, client, session).await
+        correct_spelling_check(text, sender, room_id, client, session).await
     } else {
         do_nothing().await
     }
 }
 
-async fn correct_spelling(
+async fn correct_spelling_check(
     text: &TextMessageEventContent,
     sender: &UserId,
     room_id: &RoomId,
     client: &HttpsClient,
     session: &mut SavedSession,
 ) -> Result<()> {
-    if text.body.contains("jellyfish") {
-        if sender.localpart() == session.get_username() {
-            do_nothing().await
-        } else {
-            if text.relates_to == None {
-                let response = client
-                    .request(create_message_event::Request {
-                        room_id: room_id.clone(),
-                        event_type: EventType::RoomMessage,
-                        txn_id: session.next_txn_id(),
-                        data: MessageEventContent::Text(TextMessageEventContent {
-                            body: text.body.to_string(),
-                            format: None,
-                            formatted_body: None,
-                            relates_to: None,
-                        }),
-                    })
-                    .await;
-                match response {
-                    Ok(_) => Ok(()),
-                    Err(e) => Err(Error::RumaClientError(e)),
+    if sender.localpart() == session.get_username() {
+        do_nothing().await
+    } else {
+        if text.relates_to == None {
+            for incorrect_spelling in INSENSITIVE_SPELL_CHECK.into_iter() {
+                if text
+                    .body
+                    .to_lowercase()
+                    .contains(&incorrect_spelling.to_lowercase())
+                {
+                    let response = client
+                        .request(create_message_event::Request {
+                            room_id: room_id.clone(),
+                            event_type: EventType::RoomMessage,
+                            txn_id: session.next_txn_id(),
+                            data: MessageEventContent::Text(TextMessageEventContent {
+                                body: correct_spelling(sender.localpart(), incorrect_spelling),
+                                format: None,
+                                formatted_body: None,
+                                relates_to: None,
+                            }),
+                        })
+                        .await;
+                    match response {
+                        Ok(_) => return Ok(()),
+                        Err(e) => return Err(Error::RumaClientError(e)),
+                    }
                 }
-            } else {
-                do_nothing().await
+            }
+            for incorrect_spelling in SENSITIVE_SPELL_CHECK.into_iter() {
+                if text.body.contains(incorrect_spelling) {
+                    let response = client
+                        .request(create_message_event::Request {
+                            room_id: room_id.clone(),
+                            event_type: EventType::RoomMessage,
+                            txn_id: session.next_txn_id(),
+                            data: MessageEventContent::Text(TextMessageEventContent {
+                                body: correct_spelling(sender.localpart(), incorrect_spelling),
+                                format: None,
+                                formatted_body: None,
+                                relates_to: None,
+                            }),
+                        })
+                        .await;
+                    match response {
+                        Ok(_) => return Ok(()),
+                        Err(e) => return Err(Error::RumaClientError(e)),
+                    }
+                }
             }
         }
-    } else {
-        do_nothing().await
+        return Ok(()); // No matches found, so return Ok
     }
 }
 
 async fn do_nothing() -> Result<()> {
     Ok(())
+}
+
+fn correct_spelling(user: &str, incorrect_spelling: &str) -> String {
+    format!("I'd just like to interject for a moment {}. What you're referring to as {}, is in fact, Jellyfin, or as I've recently taken to calling it, Linux plus Jellyfin. Jellyfin is not an operating system unto itself, but rather another free component of a fully functioning Linux system made useful by ffmpeg, dwarf fortress, and other vital system components comprising a full OS as defined by Steve Ballmer. Many computer users run a modified version of Jellyfin every day, without realizing it. Through a peculiar turn of events, the version of Jellyfin which is widely used today is often called \"that one media player thingy you have\", and many of its users are not aware that it is basically the Jellyfin system, developed with slave labor. There really is a Jellyfin, and these people are using it, but it is just a part of the system they use. Jellyfin is the answer: the program in the system that allocates the machine's resources until your fans are screaming for the sweet release of death. The media server is an essential part of an operating system, but useless by itself; it can only function in the context of a complete collection of Linux ISOs. Jellyfin is normally used in combination with the Linux operating system: the whole system is basically Linux with Jellyfin added, or Linux/Jellyfin. All the so-called \"Linux\" releases are really distributions of Linux/Jellyfin.", user, incorrect_spelling)
 }
