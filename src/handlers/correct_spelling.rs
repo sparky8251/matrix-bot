29ebac4c4dec04ced21d6fa68e63d06b83e42698
sparky8251 <sparky@possumlodge.me>
2020-04-07 08:35:39 -0400

    Reduced spamminess. Need to implement per room rate limiting next

--- src/handlers/correct_spelling.rs
+++ src/handlers/correct_spelling.rs
@@ -3,6 +3,8 @@ use super::helpers::do_nothing;
 use crate::error::{Error, Result};
 use crate::session::SavedSession;
 
+use std::time::SystemTime;
+
 use ruma_client::{api::r0::message::create_message_event, HttpsClient};
 use ruma_events::{
     room::message::{MessageEventContent, TextMessageEventContent},
@@ -23,55 +25,63 @@ pub(super) async fn correct_spelling_check(
     if sender.localpart() == session.get_username() {
         do_nothing().await
     } else {
-        if text.relates_to == None {
-            for incorrect_spelling in INSENSITIVE_SPELL_CHECK.into_iter() {
-                if text
-                    .body
-                    .to_lowercase()
-                    .contains(&incorrect_spelling.to_lowercase())
-                {
-                    let response = client
-                        .request(create_message_event::Request {
-                            room_id: room_id.clone(),
-                            event_type: EventType::RoomMessage,
-                            txn_id: session.next_txn_id(),
-                            data: MessageEventContent::Text(TextMessageEventContent {
-                                body: correct_spelling(sender.localpart(), incorrect_spelling),
-                                format: None,
-                                formatted_body: None,
-                                relates_to: None,
-                            }),
-                        })
-                        .await;
-                    match response {
-                        Ok(_) => return Ok(()),
-                        Err(e) => return Err(Error::RumaClientError(e)),
+        if session.correction_time_cooldown() {
+            if text.relates_to == None {
+                for incorrect_spelling in INSENSITIVE_SPELL_CHECK.into_iter() {
+                    if text
+                        .body
+                        .to_lowercase()
+                        .contains(&incorrect_spelling.to_lowercase())
+                    {
+                        let response = client
+                            .request(create_message_event::Request {
+                                room_id: room_id.clone(),
+                                event_type: EventType::RoomMessage,
+                                txn_id: session.next_txn_id(),
+                                data: MessageEventContent::Text(TextMessageEventContent {
+                                    body: correct_spelling(sender.localpart(), incorrect_spelling),
+                                    format: None,
+                                    formatted_body: None,
+                                    relates_to: None,
+                                }),
+                            })
+                            .await;
+                        match response {
+                            Ok(_) => {
+                                session.set_last_correction_time(SystemTime::now());
+                                return Ok(());
+                            }
+                            Err(e) => return Err(Error::RumaClientError(e)),
+                        }
                     }
                 }
-            }
-            for incorrect_spelling in SENSITIVE_SPELL_CHECK.into_iter() {
-                if text.body.contains(incorrect_spelling) {
-                    let response = client
-                        .request(create_message_event::Request {
-                            room_id: room_id.clone(),
-                            event_type: EventType::RoomMessage,
-                            txn_id: session.next_txn_id(),
-                            data: MessageEventContent::Text(TextMessageEventContent {
-                                body: correct_spelling(sender.localpart(), incorrect_spelling),
-                                format: None,
-                                formatted_body: None,
-                                relates_to: None,
-                            }),
-                        })
-                        .await;
-                    match response {
-                        Ok(_) => return Ok(()),
-                        Err(e) => return Err(Error::RumaClientError(e)),
+                for incorrect_spelling in SENSITIVE_SPELL_CHECK.into_iter() {
+                    if text.body.contains(incorrect_spelling) {
+                        let response = client
+                            .request(create_message_event::Request {
+                                room_id: room_id.clone(),
+                                event_type: EventType::RoomMessage,
+                                txn_id: session.next_txn_id(),
+                                data: MessageEventContent::Text(TextMessageEventContent {
+                                    body: correct_spelling(sender.localpart(), incorrect_spelling),
+                                    format: None,
+                                    formatted_body: None,
+                                    relates_to: None,
+                                }),
+                            })
+                            .await;
+                        match response {
+                            Ok(_) => {
+                                session.set_last_correction_time(SystemTime::now());
+                                return Ok(());
+                            }
+                            Err(e) => return Err(Error::RumaClientError(e)),
+                        }
                     }
                 }
             }
         }
-        return Ok(()); // No matches found, so return Ok
+        return Ok(()); // No matches found or cooldown time not met, so return Ok
     }
 }
 
--- src/session.rs
+++ src/session.rs
@@ -1,4 +1,5 @@
 use std::fs::{self, File};
+use std::time::{Duration, SystemTime};
 
 use log::info;
 use ruma_client::Session;
@@ -15,6 +16,7 @@ pub struct SavedSession {
     last_sync: Option<String>,
     active_room: Option<RoomId>,
     last_txn_id: u64,
+    last_correction_time: Option<SystemTime>,
 }
 
 impl SavedSession {
@@ -53,9 +55,29 @@ impl SavedSession {
         self.session = Some(session)
     }
 
-    // FIXME: This needs to be an idempotent/unique ID to be spec compliant
+    pub fn set_last_correction_time(&mut self, time: SystemTime) {
+        self.last_correction_time = Some(time)
+    }
+
+    // FIXME: This needs to be an idempotent/unique ID per txn to be spec compliant
     pub fn next_txn_id(&mut self) -> String {
         self.last_txn_id += 1;
         self.last_txn_id.to_string()
+    }
+
+    pub fn correction_time_cooldown(&self) -> bool {
+        match self.last_correction_time {
+            Some(v) => match v.elapsed() {
+                Ok(v) => {
+                    if v >= Duration::new(300, 0) {
+                        true
+                    } else {
+                        false
+                    }
+                }
+                Err(_) => false,
+            },
+            None => true, // Will only be None if this session has not yet corrected anyone, so return true to allow correction
+        }
     }
 }
