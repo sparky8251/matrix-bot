2bea62a63e9407f476505d025be3cf7a9b965d9d
sparky8251 <sparky@possumlodge.me>
2020-05-11 14:20:56 -0400

    fixed case in which regex would not match single digit values for conversion

--- src/regex.rs
+++ src/regex.rs
@@ -9,7 +9,7 @@ lazy_static! {
         r"(?x)
         ^!convert                           # The tag from line start
         \s*?                                # Any amount of whitespace
-        ([[:digit:]]+\.{0,1}[[:digit:]]+)   # The number to convert, will only allow 1 period for floating points (captured)
+        ([[:digit:]]+\.{0,1}[[:digit:]]*)   # The number to convert, will only allow 1 period for floating points (captured)
         \s*?                                # Any amount of white space
         ([[:alpha:]/]+)                     # The unit to convert from including potential / (captured)
     "
@@ -19,7 +19,7 @@ lazy_static! {
 lazy_static! {
     pub static ref UNIT_CONVERSION: Regex = Regex::new(
         r"(?x)
-        ([[:digit:]]+\.{0,1}[[:digit:]]+)   # The number to convert, will only allow 1 period for floating points (captured)
+        ([[:digit:]]+\.{0,1}[[:digit:]]*)   # The number to convert, will only allow 1 period for floating points (captured)
         \s*?                                # Any amount of white space
         ([[:alpha:]/]+)                     # The unit to convert from including potential / (captured)
     "
