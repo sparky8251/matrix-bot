2e3c1f4310f320fbde4247a95d8407aa803170b0
sparky8251 <sparky@possumlodge.me>
2020-05-29 13:16:30 -0400

    Attempt fix of test for use with CI

--- src/queries/tests/common.rs
+++ src/queries/tests/common.rs
@@ -2,22 +2,27 @@ use std::fs::File;
 use std::io::{ErrorKind, Read};
 
 pub(super) fn load_access_token() -> String {
-    let mut file = match File::open(".access_token") {
+    match std::env::var("CI_TEST_TOKEN") {
         Ok(v) => v,
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => panic!("Unable to find file .access_token"),
+        Err(_) => {
+            let mut file = match File::open(".access_token") {
+                Ok(v) => v,
+                Err(e) => match e.kind() {
+                    ErrorKind::NotFound => panic!("Unable to find file .access_token"),
 
-            ErrorKind::PermissionDenied => {
-                panic!("Permission denied when opening file .access_token")
+                    ErrorKind::PermissionDenied => {
+                        panic!("Permission denied when opening file .access_token")
+                    }
+
+                    _ => panic!("Unable to open file due to unexpected error {:?}", e),
+                },
+            };
+            let mut contents = String::new();
+            match file.read_to_string(&mut contents) {
+                Ok(_) => (), // If read is successful, do nothing
+                Err(e) => panic!("Unable to read file contents due to error {:?}", e),
             }
-
-            _ => panic!("Unable to open file due to unexpected error {:?}", e),
-        },
-    };
-    let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Ok(_) => (), // If read is successful, do nothing
-        Err(e) => panic!("Unable to read file contents due to error {:?}", e),
+            contents
+        }
     }
-    contents
 }
