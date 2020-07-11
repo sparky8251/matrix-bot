f53688b5c18a8cdd497ad8539534c4e778b638e0
sparky8251 <sparky@possumlodge.me>
2020-07-11 15:45:21 -0400

    Fixed bug that caused text in inline and code blocks to be processed

--- src/helpers/clean_text.rs
+++ src/helpers/clean_text.rs
@@ -14,7 +14,7 @@ pub fn clean_text(text: &str) -> String {
     let clean_text = PRE_TAG.replace_all(&clean_text, "");
     trace!("Cleaned text after pre tag is {:?}", clean_text);
 
-    let clean_text = MX_REPLY.replace_all(&text, "");
+    let clean_text = MX_REPLY.replace_all(&clean_text, "");
     trace!("Cleaned text after removing reply is {:?}", clean_text);
 
     clean_text.to_string()
